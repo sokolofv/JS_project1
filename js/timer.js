12bba631d83ce2453ee282ec306525ace115fb62
sokol_ofv <anatoliy.sokolofv@gmail.com>
2025-11-11 16:19:55 +0300

    done

---
+++ js/timer.js
@@ -0,0 +1,62 @@
+const daysBlock = document.querySelector('.timer__days')
+const hoursBlock = document.querySelector('.timer__hours')
+const minutesBlock = document.querySelector('.timer__minutes')
+const secondsBlock = document.querySelector('.timer__seconds')
+
+let interval 
+
+const numWord = (value, words) => {
+    value = Math.abs(value) % 100
+    const lastNum = value % 10
+
+    if (value > 10 & value < 20) return words[2]
+    if ( lastNum > 1 & lastNum < 5) return words[1]
+    if ( lastNum === 1) return words[0]
+    return words[2];
+}
+
+const updateTimer = () => {
+    const date = new Date()
+    const dateDeadline = new Date('12 november 2025').getTime()
+    const timeRemaining = (dateDeadline - date) / 1000
+
+    const days = Math.floor(timeRemaining / 60 / 60 / 24)    
+    const hours = Math.floor(timeRemaining / 60 / 60 % 24)
+    const minutes = Math.floor((timeRemaining / 60) % 60)
+    const seconds = Math.floor(timeRemaining % 60)
+
+    const fDays = days < 10 ? '0' + days : days
+    const fHours = hours < 10 ? '0' + hours : hours
+    const fMinutes = minutes < 10 ? '0' + minutes : minutes
+    const fSeconds = seconds < 10 ? '0' + seconds : seconds
+
+    daysBlock.textContent = fDays
+    daysBlock.nextElementSibling.textContent = numWord(hours, ['день', 'дня', 'дней'])
+    hoursBlock.textContent = fHours
+    hoursBlock.nextElementSibling.textContent = numWord(hours, ['час', 'часа', 'часов'])
+    minutesBlock.textContent = fMinutes
+    minutesBlock.nextElementSibling.textContent = numWord(minutes, ['минута', 'минуты', 'минут'])
+    secondsBlock.textContent = fSeconds
+    secondsBlock.nextElementSibling.textContent = numWord(seconds, ['секунда', 'секунды', 'секунд'])
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval)
+        daysBlock.textContent = '00'
+        hoursBlock.textContent = '00'
+        minutesBlock.textContent = '00'
+        secondsBlock.textContent = '00'
+
+        daysBlock.parentElement.classList.add('timer__item--expired');
+        hoursBlock.parentElement.classList.add('timer__item--expired');
+        minutesBlock.parentElement.classList.add('timer__item--expired');
+        secondsBlock.parentElement.classList.add('timer__item--expired');
+    }
+}
+
+updateTimer()
+interval = setInterval(updateTimer, 500)
+
+
+
+
+
